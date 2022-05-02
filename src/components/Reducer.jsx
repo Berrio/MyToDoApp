ac975f886af497693912cec6970ed29d628a1fa9
JonatanBerrioRoman <berrioromanjonatan@gmail.com>
2022-05-02 15:16:02 -0500

    ading reducer

---
+++ src/components/Reducer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function reducer(state,action){
+    switch(action.type){
+        case 'add-note':
+            return state
+        case 'remove-note':
+            return state
+        case 'update-note':
+            return state
+            
+    }
+}
+
+export default reducer;
